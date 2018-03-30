4091306946e42a035d12c39decea2e7cb68edb56
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2018-03-30 23:44:02 +0200

    Just added some vars.

--- menu.js
+++ menu.js
@@ -1,9 +1,9 @@
 function attachMenus(selector) {
     $.getJSON('https://techbookreader.com/beta/api/menu', function (menus) {
         var menusHtml = '';
-        for (m = 0; m < menus.length; m++) {
+        for (var m = 0; m < menus.length; m++) {
             var menuHtml = '<li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' + menus[m].menu + '</a><div class="dropdown-menu" aria-labelledby="navbarDropdown">';
-            for (i = 0; i < menus[m].items.length; i++) {
+            for (var i = 0; i < menus[m].items.length; i++) {
                 var item = menus[m].items[i];
                 if (item.hasOwnProperty('item')) {
                     menuHtml += '<a class="dropdown-item" href="#">' + item.item + '</a>'
