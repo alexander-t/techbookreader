0183d64b4233ca1f6685ba05468f360d0d098722
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2018-03-30 23:44:14 +0200

    New simple router.

---
+++ router.js
@@ -0,0 +1,28 @@
+var Router = (function () {
+    var routes = [];
+
+    var add = function (route, handler) {
+        routes.push({route: route, handler: handler});
+    };
+
+    var update = function () {
+        var fragment = getFragment();
+        for (var i = 0; i < routes.length; i++) {
+            var match = fragment.match(routes[i].route + '$');
+            if (match) {
+                match.shift();
+                routes[i].handler.apply({}, match);
+            }
+        }
+    };
+
+    var getFragment = function () {
+        var match = window.location.href.match(/(#.*)$/);
+        return match ? match[1] : '';
+    };
+
+    return {
+        add: add,
+        update: update
+    };
+})();
