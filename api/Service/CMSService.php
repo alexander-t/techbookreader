c23978fe2b6de23badcf9b395511c30d80bf6035
Alexander Tarlinder <a@tarlinder.net>
2018-04-08 01:40:47 +0200

    Service that saves reviews.

---
+++ api/Service/CMSService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace TechbookReader\Service;
+use PDO;
+
+class CMSService {
+    private $pdo;
+
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function saveReview($review) {
+        $this->pdo->prepare('UPDATE reviews SET title=:title,author=:author,publication_year=:publication_year,reviewed=:reviewed,is_classic=:is_classic,opinion=:opinion,summary=:summary WHERE id=:id')
+                  ->execute(
+            ['id' => $review['id'],
+            'title' => $review['title'],
+            'author' => $review['author'],
+            'publication_year' => $review['publication_year'],
+            'reviewed' => $review['reviewed'],
+            'is_classic' => $review['is_classic'] == true ? 1 : 0,
+            'opinion' => $review['opinion'],
+            'summary' => $review['summary']
+            ]);
+    }
+}
