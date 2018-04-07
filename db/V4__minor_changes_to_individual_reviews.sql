1b320f69f5893d25c8ccce705e0402e3525399d2
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2018-04-07 09:02:55 +0200

    Removed db directory because it has been moved to another project.

--- db/V4__minor_changes_to_individual_reviews.sql
+++
@@ -1,4 +0,0 @@
-UPDATE reviews SET title = 'Refactoring: Improving the Design of Existing Code' where title='Refactoring';
-UPDATE reviews SET summary=NULL WHERE title LIKE 'Professional Scrum Development%';
-UPDATE reviews SET image='51+iMbZgUDL._SL250_.jpg' WHERE title LIKE 'Sun Certified Web Component Developer Study Companion: SCWCD Java EE 5%';
-
