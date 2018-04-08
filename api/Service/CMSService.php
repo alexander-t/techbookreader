86fb2563257d14d43c56af021625c2b12112c672
Alexander Tarlinder <a@tarlinder.net>
2018-04-08 22:24:42 +0200

    Set canonical_title on update.

--- api/Service/CMSService.php
+++ api/Service/CMSService.php
@@ -13,7 +13,7 @@ class CMSService
 
     public function saveReview($review)
     {
-        $this->pdo->prepare('UPDATE reviews SET title=:title,author=:author,publication_year=:publication_year,reviewed=:reviewed,is_classic=:is_classic,opinion=:opinion,summary=:summary WHERE id=:id')
+        $this->pdo->prepare('UPDATE reviews SET title=:title,author=:author,publication_year=:publication_year,reviewed=:reviewed,is_classic=:is_classic,opinion=:opinion,summary=:summary,canonical_title=:canonical_title WHERE id=:id')
             ->execute(
                 ['id' => $review['id'],
                     'title' => $review['title'],
@@ -22,7 +22,14 @@ class CMSService
                     'reviewed' => $review['reviewed'],
                     'is_classic' => $review['is_classic'] == true ? 1 : 0,
                     'opinion' => $review['opinion'],
-                    'summary' => $review['summary']
+                'summary' => $review['summary'],
+                'canonical_title' => $this->canonicalize($review['title'])
                 ]);
+    }
+
+    private function canonicalize($s) {
+        $r = trim(strtolower($s));
+        $r = preg_replace("/\s+/", "_", $r);
+        return preg_replace("/[^a-z0-9_]/", "", $r);
     }
 }
